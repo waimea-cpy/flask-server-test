8ea399cc38fbe3cd17545d23459cdf22eca42695
Steve Copley <steve.copley@waimea.school.nz>
2024-10-06 20:29:52 +1300

    Tidy up

--- app/__init__.py
+++ app/__init__.py
@@ -20,17 +20,19 @@ def create_app():
     app.config['UPLOADS_DIR'] = 'uploads'
 
     # Prevent cookies being retained after browser close
-    app.config["SESSION_PERMANENT"] = False
+    app.config['SESSION_PERMANENT'] = False
 
     # Routing blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # Setup DB and Files handlers
     from . import db
     db.init_app(app)
+    
     from . import files
     files.init_app(app)
 
