3f6dc9840bb0daeb5e45a8e7ae1e80f866bff65e
Steve Copley <steve.copley@waimea.school.nz>
2024-10-09 22:30:14 +1300

    Formatting

--- app/data/schema.sql
+++ app/data/schema.sql
@@ -1,12 +1,14 @@
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `things`;
 
+
 CREATE TABLE users (
     `id`       INTEGER PRIMARY KEY AUTOINCREMENT,
     `name`     TEXT UNIQUE NOT NULL,
     `username` TEXT UNIQUE NOT NULL,
     `hash`     TEXT UNIQUE NOT NULL
 );
+
 
 CREATE TABLE things (
     `id`    INTEGER PRIMARY KEY AUTOINCREMENT,
